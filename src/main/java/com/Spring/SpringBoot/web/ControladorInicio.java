9bdec4997524ea45ba8765ba7dd85fb78fc4fe23
RExequiel <exe2016gerez@gmail.com>
2023-02-18 23:18:51 -0300

    completando CRUD

--- src/main/java/com/Spring/SpringBoot/web/ControladorInicio.java
+++ src/main/java/com/Spring/SpringBoot/web/ControladorInicio.java
@@ -1,5 +1,6 @@
 package com.Spring.SpringBoot.web;
 
+import com.Spring.SpringBoot.domain.Cliente;
 import com.Spring.SpringBoot.servicio.ClienteService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -23,5 +25,25 @@ public class ControladorInicio {
 
         model.addAttribute("clientes", clientes);
         return "index";
+    }
+    @GetMapping("/agregar")
+    public String agregar(Cliente cliente){
+        return "modificar";
+    }
+    @PostMapping("/guardar")
+    public String guardar(Cliente cliente){
+        clienteService.guardar(cliente);
+        return "redirect:/";
+    }
+    @GetMapping("/editar/{id_cliente}")
+    public String editar(Cliente cliente, Model model){
+        cliente = clienteService.encontrarCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificar";
+    }
+    @GetMapping("/eliminar")
+    public String eliminar(Cliente cliente){
+        clienteService.eliminar(cliente);
+        return "redirect:/";
     }
 }
