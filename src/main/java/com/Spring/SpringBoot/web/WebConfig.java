72a3fe228947a0fa2a154b1c2ad0b5dca1ebdc9a
RExequiel <exe2016gerez@gmail.com>
2023-02-19 19:30:14 -0300

    agregando internacionalización (i18n)

---
+++ src/main/java/com/Spring/SpringBoot/web/WebConfig.java
@@ -0,0 +1,30 @@
+package com.Spring.SpringBoot.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver(){
+        var srl = new SessionLocaleResolver();
+        srl.setDefaultLocale(new Locale("es"));
+        return srl;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+}
